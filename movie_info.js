c0b912d00ecd3cad7a2697ac56e549fcd7e35ee8
Huseyin Yilmaz <ylmzhsyn98@gmail.com>
2020-09-11 19:14:52 +0300

    imdb id control added

--- movie_info.js
+++ movie_info.js
@@ -72,6 +72,7 @@ module.exports.movie_info = (req, res, db) => {
     if (!/tt[0-9]*/.test(movie_id)) {
         response.status = "parsing_error";
         res.status(400).json(response);
+        return;
     }
 
 
